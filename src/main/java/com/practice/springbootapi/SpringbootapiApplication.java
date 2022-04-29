8abbed4bd9dd43fce16cac4f04e8d879cb205191
qkq <qkqxhd200@naver.com>
2022-04-29 17:56:41 +0900

    first commit

---
+++ src/main/java/com/practice/springbootapi/SpringbootapiApplication.java
@@ -0,0 +1,13 @@
+package com.practice.springbootapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootapiApplication.class, args);
+	}
+
+}
